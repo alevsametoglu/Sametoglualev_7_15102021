3b6599b9c14891d86c2700148bb83ac7e52b767b
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-22 09:42:41 +0200

    create filter buttons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
 import api from './api.js';
+import DropdownButton from './components/DropdownButton.js';
 
-// const utensils = api.getUtensils()
-// console.log(utensils)
-// const ingredient = api.getIngredients();
+const filterSection = document.querySelector('section.filter-tags');
+
+const selectFilterOption = (id, option) => {
+    console.log(id, '-', option);
+};
+
+// create ingredients filter button
+const ingredients = api.getIngredients();
+const dropdownButtonIngredients = new DropdownButton(
+    'ingredient',
+    'Ingredients',
+    'btn-primary',
+    'ingredient',
+    ingredients,
+    selectFilterOption,
+);
+filterSection.appendChild(dropdownButtonIngredients.el);
+
+// create utils filter button
 const utils = api.getUtils();
+const dropdownUtils = new DropdownButton('util', 'Appareil', 'btn-success', 'appareil', utils, selectFilterOption);
+filterSection.appendChild(dropdownUtils.el);
+
+// create utensils filter button
+
+const utensils = api.getUtensils();
+const dropdownUtensils = new DropdownButton(
+    'utensils',
+    'Ustensiles',
+    'btn-danger',
+    'ustensiles',
+    utensils,
+    selectFilterOption,
+);
+filterSection.appendChild(dropdownUtensils.el);
