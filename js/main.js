7e6a943be51c83a6306227efb464b7315abfc297
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-27 09:40:49 +0200

    uptade tag list

--- js/main.js
+++ js/main.js
@@ -1,38 +1,85 @@
 import api from './api.js';
 import DropdownButton from './components/DropdownButton.js';
+import TagList from './components/TagList.js';
+import RecipeCard from './components/RecipesCard.js';
 
-const filterSection = document.querySelector('section.filter-tags');
-
-const selectFilterOption = (id, option) => {
-    console.log(id, '-', option);
+const selectedTagsChanged = () => {
+    console.log(tagListIngredientEl.selectedTags);
+    console.log(tagListUtilEl.selectedTags);
+    console.log(tagListUtensilEl.selectedTags);
 };
 
-// create ingredients filter button
-const ingredients = api.getIngredients();
-const dropdownButtonIngredients = new DropdownButton(
-    'ingredient',
-    'Ingredients',
-    'btn-primary',
-    'ingredient',
-    ingredients,
-    selectFilterOption,
-);
-filterSection.appendChild(dropdownButtonIngredients.el);
+const tagListIngredientEl = new TagList('primary', selectedTagsChanged);
+const tagListUtilEl = new TagList('success', selectedTagsChanged);
+const tagListUtensilEl = new TagList('danger', selectedTagsChanged);
 
-// create utils filter button
-const utils = api.getUtils();
-const dropdownUtils = new DropdownButton('util', 'Appareil', 'btn-success', 'appareil', utils, selectFilterOption);
-filterSection.appendChild(dropdownUtils.el);
+const initTagList = () => {
+    const tagSectionEl = document.getElementById('selected-tags');
+    tagSectionEl.appendChild(tagListIngredientEl.el);
+    tagSectionEl.appendChild(tagListUtilEl.el);
+    tagSectionEl.appendChild(tagListUtensilEl.el);
+};
 
-// create utensils filter button
+// create filter buttons function
+const initFilterButtons = () => {
+    const filterSection = document.querySelector('section.filter-tags');
 
-const utensils = api.getUtensils();
-const dropdownUtensils = new DropdownButton(
-    'utensils',
-    'Ustensiles',
-    'btn-danger',
-    'ustensiles',
-    utensils,
-    selectFilterOption,
-);
-filterSection.appendChild(dropdownUtensils.el);
+    // create ingredients filter button
+    const ingredients = api.getIngredients();
+    const itemClickedIngredient = (item) => tagListIngredientEl.addTag(item);
+
+    const filterDropdownIngredientItems = (inputValue) => {
+        const ingredients = api.getIngredients(inputValue);
+        dropdownButtonIngredients.updateOptionList(ingredients);
+    };
+    const dropdownButtonIngredients = new DropdownButton(
+        'Ingredients',
+        'primary',
+        ingredients,
+        itemClickedIngredient,
+        filterDropdownIngredientItems,
+    );
+    filterSection.appendChild(dropdownButtonIngredients.el);
+
+    // create utils filter button
+    const utils = api.getUtils();
+    const itemClickedUtils = (item) => tagListUtilEl.addTag(item);
+
+    const filterDropdownUtilsItems = (inputValue) => {
+        const utils = api.getUtils(inputValue);
+        dropdownUtils.updateOptionList(utils);
+    };
+
+    const dropdownUtils = new DropdownButton('Appareil', 'success', utils, itemClickedUtils, filterDropdownUtilsItems);
+    filterSection.appendChild(dropdownUtils.el);
+
+    // create utensils filter button
+    const utensils = api.getUtensils();
+    const itemClickedUtensil = (item) => tagListUtensilEl.addTag(item);
+    const filterDropdownUtensilsItems = (inputValue) => {
+        const utensils = api.getUtils(inputValue);
+        dropdownUtensils.updateOptionList(utensils);
+    };
+    const dropdownUtensils = new DropdownButton(
+        'Ustensiles',
+        'danger',
+        utensils,
+        itemClickedUtensil,
+        filterDropdownUtensilsItems,
+    );
+    filterSection.appendChild(dropdownUtensils.el);
+};
+
+const initRecipeList = () => {
+    const recipeListSection = document.querySelector('#recipes-list');
+    const recipes = api.getRecipes();
+    // console.log(recipes);
+    recipes.forEach((recipe) => {
+        const recipeEl = new RecipeCard(recipe.name, recipe.time, recipe.ingredients, recipe.description);
+        recipeListSection.appendChild(recipeEl.el);
+    });
+};
+
+initFilterButtons();
+initTagList();
+initRecipeList();
