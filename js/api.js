0cd6c63c7f15672cd791bf8a1e2051ab297c11fb
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-27 09:40:10 +0200

    add filtered tags

--- js/api.js
+++ js/api.js
@@ -1,18 +1,25 @@
 import recipes from './recipes.js';
 
 class API {
-    static getUtensils() {
+    static getUtensils(searchKey) {
         const uniqUtensils = [];
 
         const utensils = recipes.map((recipe) => recipe.ustensils).flat();
         utensils.forEach((utensil) => {
             if (!uniqUtensils.includes(utensil)) uniqUtensils.push(utensil);
         });
-
-        return uniqUtensils;
+        if (!searchKey) return uniqUtensils;
+        else {
+            searchKey = searchKey.toLowerCase();
+            const filteredUtensils = uniqUtensils.filter((utensils) => {
+                const isIncude = utensil.toLowerCase().includes(searchKey);
+                return isIncude;
+            });
+            return filteredUtils;
+        }
     }
 
-    static getIngredients() {
+    static getIngredients(searchKey) {
         const uniqIngredients = [];
         const ingredients = recipes.map((recipe) => recipe.ingredients).flat();
 
@@ -20,16 +27,47 @@ class API {
             if (!uniqIngredients.includes(item.ingredient)) uniqIngredients.push(item.ingredient);
         });
 
-        return uniqIngredients;
+        if (!searchKey) return uniqIngredients;
+        else {
+            searchKey = searchKey.toLowerCase();
+            const filteredIngredients = uniqIngredients.filter((ingredient) => {
+                const isInclude = ingredient.toLowerCase().includes(searchKey);
+                return isInclude;
+            });
+            return filteredIngredients;
+        }
     }
 
-    static getUtils = () => {
+    static getUtils = (searchKey) => {
         const uniqUtils = [];
         const utils = recipes.map((recipe) => recipe.appliance);
         utils.forEach((util) => {
             if (!uniqUtils.includes(util)) uniqUtils.push(util);
         });
-        return uniqUtils;
+        if (!searchKey) return uniqUtils;
+        else {
+            searchKey = searchKey.toLowerCase();
+            const filteredUtils = uniqUtils.filter((util) => {
+                const isInclude = util.toLowerCase().includes(searchKey);
+                return isInclude;
+            });
+            return filteredUtils;
+        }
+    };
+
+    static getRecipes = (filterParams) => {
+        if (!filterParams) return recipes;
+        const { utils, ingredients, utensils, searchKey } = filterParams;
+
+        if (!utils && !ingredients && !utensils && !searchKey) return recipes;
+
+        const filteredRecipes = [];
+
+        recipes.forEach((recipe) => {
+            //TODO: filter recipe with filter params and push to filtered recipes array
+        });
+
+        return filteredRecipes;
     };
 }
 
