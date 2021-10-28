08d9e4af5067b93179da3b02d8a20eecdd319f77
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-10-28 14:11:31 +0200

    filtered by tag

--- js/api.js
+++ js/api.js
@@ -56,15 +56,32 @@ class API {
     };
 
     static getRecipes = (filterParams) => {
+        console.log(filterParams);
         if (!filterParams) return recipes;
         const { utils, ingredients, utensils, searchKey } = filterParams;
 
         if (!utils && !ingredients && !utensils && !searchKey) return recipes;
+        if (utils.length === 0 && ingredients.length === 0 && utensils.length === 0) return recipes;
 
         const filteredRecipes = [];
 
         recipes.forEach((recipe) => {
             //TODO: filter recipe with filter params and push to filtered recipes array
+
+            const isIngredientsExist = ingredients.every((ingredient) =>
+                recipe.ingredients.some((x) => x.ingredient === ingredient),
+            );
+            const isUtilsExist = utils.every((util) => recipe.appliance.includes(util));
+
+            const isUtensilsExist = utensils.every((utensil) => recipe.ustensils.includes(utensil));
+
+            // filtered tag by tag
+            // if (isIngredientsExist) filteredRecipes.push(recipe);
+            // if (isUtilsExist) filteredRecipes.push(recipe);
+            // if (isUtensilsExist) filteredRecipes.push(recipe);
+
+            // filtered
+            if (isUtilsExist && isIngredientsExist && isUtensilsExist) filteredRecipes.push(recipe);
         });
 
         return filteredRecipes;
